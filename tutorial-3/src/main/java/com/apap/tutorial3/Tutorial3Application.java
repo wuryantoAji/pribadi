9884a460e0387e08452340589937c03dc8a575cc
aji wuryanto <ajiw4056@gmail.com>
2018-09-26 20:17:53 +0700

    tutorial-3

---
+++ tutorial-3/src/main/java/com/apap/tutorial3/Tutorial3Application.java
@@ -0,0 +1,12 @@
+package com.apap.tutorial3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutorial3Application.class, args);
+	}
+}
